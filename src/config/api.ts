0b2e8384e7cf163ce1846774f68a8e996ac7c83d
Gema Loperena <gema.loperena@gmail.com>
2026-08-27 11:46:13 -0600

    add new port for backend

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 export const API_ENDPOINTS = {
-    MAIN: (import.meta as any).env.VITE_API_MAIN || 'http://10.106.1.49:6000/api',
-    SECONDARY: (import.meta as any).env.VITE_API_SECONDARY || 'http://10.106.1.49:6000/api',
-    TERTIARY: (import.meta as any).env.VITE_API_TERTIARY || 'http://10.106.1.49:6000/api',
+    MAIN: (import.meta as any).env.VITE_API_MAIN || 'http://10.106.1.49:6001/api',
+    SECONDARY: (import.meta as any).env.VITE_API_SECONDARY || 'http://10.106.1.49:6001/api',
+    TERTIARY: (import.meta as any).env.VITE_API_TERTIARY || 'http://10.106.1.49:6001/api',
 };
 // export const API_ENDPOINTS = {
 //     MAIN: (import.meta as any).env.VITE_API_MAIN || 'http://localhost:9000/api',
